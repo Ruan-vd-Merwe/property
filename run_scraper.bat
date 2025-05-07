82451757285a839cc0defeeabda92d1a19bd1a31
Ruan-vd-Merwe <ruanvandermerwe343@gmail.com>
2025-05-07 13:28:29 +0200

    clear

--- run_scraper.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\Users\RuanvanderMerwe\Documents\Code\Python_Training\prop24-scrape\git_streamlit_prop24\property-1"
-"C:\Users\RuanvanderMerwe\AppData\Local\Programs\Python\Python312\python.exe" 1_property_urls.py
