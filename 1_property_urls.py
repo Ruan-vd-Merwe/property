de5791a8318fc3a60e8aa4a0c6b2d25da1024346
Ruan-vd-Merwe <97937352+Ruan-vd-Merwe@users.noreply.github.com>
2025-05-07 15:23:11 +0200

    Update 1_property_urls.py

--- 1_property_urls.py
+++ 1_property_urls.py
@@ -16,12 +16,18 @@ import tempfile
 
 
 options = Options()
+options.add_argument("--C:\Users\RuanvanderMerwe\Documents\Code\Python_Training\prop24-scrape\git_streamlit_prop24\property-1\chrome_user_data")
+
+driver = webdriver.Chrome(service=Service(), options=options)
+
+
+options = Options()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
 # Use a unique, temporary user data dir to avoid profile lock errors
-options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+# options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
 # Use headless only if needed; comment out for debugging
 # options.add_argument("--headless=new")
