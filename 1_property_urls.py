b56b709c8a660c1393af82c24cbdd1628b138299
Ruan-vd-Merwe <ruanvandermerwe343@gmail.com>
2025-05-15 11:33:37 +0200

    s

--- 1_property_urls.py
+++ 1_property_urls.py
@@ -1,34 +1,17 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager  # NEW
+
 from datetime import datetime
 import csv
 import time
 import random
 
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-extensions')
-options.add_argument('--start-maximized')
-# Memory optimization
-options.add_argument('--disk-cache-size=1')
-options.add_argument('--media-cache-size=1')
-options.add_argument('--incognito')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--aggressive-cache-discard')
 
-service = Service(r"C:\Users\RuanvanderMerwe\.wdm\drivers\chromedriver\win64\135.0.7049.114\chromedriver-win32\chromedriver.exe")
-driver = webdriver.Chrome(service=service, options=options)
+
+# service = Service(r"C:\Users\RuanvanderMerwe\.wdm\drivers\chromedriver\win64\135.0.7049.114\chromedriver-win32\chromedriver.exe")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 # Go to first page to detect total number of pages
