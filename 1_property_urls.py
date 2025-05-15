678419800599a563ab1956acedccf1b83152aeac
Ruan-vd-Merwe <ruanvandermerwe343@gmail.com>
2025-05-15 11:19:59 +0200

    s

--- 1_property_urls.py
+++ 1_property_urls.py
@@ -7,11 +7,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 from datetime import datetime
 import csv
 import time
 import random
+
 
 options = Options()
 options.add_argument('--headless')
