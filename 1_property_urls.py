1ebdc373281ebeb0037e8e190ba83b623c958cb3
Ruan-vd-Merwe <ruanvandermerwe343@gmail.com>
2025-05-15 09:27:44 +0200

    d

--- 1_property_urls.py
+++ 1_property_urls.py
@@ -11,14 +11,21 @@ import time
 import random
 
 # Configure browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-options.add_argument("--headless")  # Optional: for GitHub Actions or server environments
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-# driver = webdriver.Chrome(service=Service(), options=options)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
 
 
 # Go to first page to detect total number of pages
